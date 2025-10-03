92edfe6a85e59ae8040ee03857e798a1a41bfaab
Glen <njihiaglen@gmail.com>
2025-10-03 04:25:15 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/vawoco-vision-web' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
